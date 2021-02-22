790212cda40f53f6e16819e5af5c40712383845f
Rishi <rishi.wadhwa@warriorlife.net>
2021-02-21 19:56:51 -0800

    quick fixes

--- MentalHealthProject/View Controllers/Journal/JournalBGViewController.swift
+++ MentalHealthProject/View Controllers/Journal/JournalBGViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 
-var journalBGNum = 0
 var bg = 0
 
 class JournalBGViewController: UIViewController {
@@ -19,47 +18,38 @@ class JournalBGViewController: UIViewController {
     }
     
     @IBAction func journal1Press(_ sender: Any) {
-        journalBGNum = 1
         bg = 1
     }
     
     @IBAction func journal2Press(_ sender: Any) {
-        journalBGNum = 2
         bg = 2
     }
     
     @IBAction func journal3Press(_ sender: Any) {
-        journalBGNum = 3
         bg = 3
     }
     
     @IBAction func journal4Press(_ sender: Any) {
-        journalBGNum = 4
         bg = 4
     }
     
     @IBAction func journal5Press(_ sender: Any) {
-        journalBGNum = 5
         bg = 5
     }
     
     @IBAction func journal6Press(_ sender: Any) {
-        journalBGNum = 6
         bg = 6
     }
 
     @IBAction func journal7Press(_ sender: Any) {
-        journalBGNum = 7
         bg = 7
     }
     
     @IBAction func journal8Press(_ sender: Any) {
-        journalBGNum = 8
         bg = 8
     }
     
     @IBAction func journal9Press(_ sender: Any) {
-        journalBGNum = 9
         bg = 9
     }
 
