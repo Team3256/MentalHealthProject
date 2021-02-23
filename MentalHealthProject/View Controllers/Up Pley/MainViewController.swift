66175075ddaec671b3335e7013c5fe561ec91000
Rishi <rishi.wadhwa@warriorlife.net>
2021-02-22 20:06:25 -0800

    editing journaling stuff

---
+++ MentalHealthProject/View Controllers/Up Pley/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  MentalHealthProject
+//
+//  Created by Rishi Wadhwa on 2/22/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
